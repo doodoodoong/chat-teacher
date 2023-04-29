d2bdb28f26a99f1bf4b37db18600151c94cb72f5
doodoodoong <doo_doong@hanmail.net>
2023-04-29 14:30:38 +0900

    chat API response

--- src/components/ChatGpt.js
+++ src/components/ChatGpt.js
@@ -1,10 +1,31 @@
-import { Text } from '@chakra-ui/react';
+import { Box, Button, Input, Text, VStack } from '@chakra-ui/react';
+import openai from './Openai';
+import { useState } from 'react';
 
 function ChatGpt() {
+  const [prompt, setPromprt] = useState('');
+  const [res, setRes] = useState('');
+  const handlePromptChange = e => {
+    setPromprt(e.target.value);
+  };
+  const handleSubmit = async () => {
+    const result = await openai(prompt);
+    setRes(result);
+  };
+
   return (
-    <Text color={'white'} fontSize={'7xl'} fontFamily={'mono'}>
-      Chat
-    </Text>
+    <Box>
+      <VStack spacing={4}>
+        <Input
+          value={prompt}
+          onChange={handlePromptChange}
+          placeholder="Enter your prompt"
+          color={'white'}
+        />
+        <Button onClick={handleSubmit}>Submit</Button>
+        <Text color={'white'}>{res}</Text>
+      </VStack>
+    </Box>
   );
 }
 
---
+++ src/components/Openai.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const openai = async prompt => {
+  const res = await axios.post(
+    'https://api.openai.com/v1/completions',
+    {
+      model: 'text-davinci-003',
+      prompt,
+      max_tokens: 256,
+      temperature: 0.7,
+      top_p: 1,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`,
+      },
+    }
+  );
+  return res.data.choices[0].text.trim();
+};
+
+export default openai;
