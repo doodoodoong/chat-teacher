e36e276bb924dfa716ae21bd620b22e0614495f8
doodoodoong <doo_doong@hanmail.net>
2023-05-07 13:54:33 +0900

    terminal test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,6 @@ import VideoBackground from '../components/VideoBackground';
 import VideoOverlay from '../components/VideoOverlay';
 import Header from '../components/Header';
 import Main from '../components/Main';
-import Test from '../components/Test';
 
 function Home() {
   return (
@@ -13,7 +12,6 @@ function Home() {
       <Box zIndex={1}>
         <Header />
         <Main />
-        <Test />
       </Box>
     </>
   );
