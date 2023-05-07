f2dafb1392fb6a82bf142a91327533ab6960e964
doodoodoong <doo_doong@hanmail.net>
2023-05-07 21:02:30 +0900

    spinner 추가

--- src/components/ChatGpt.js
+++ src/components/ChatGpt.js
@@ -1,16 +1,19 @@
-import { Box, Button, Input, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Input, Spinner, Text, VStack } from '@chakra-ui/react';
 import openai from './Openai';
 import { useState } from 'react';
 
 function ChatGpt() {
   const [prompt, setPromprt] = useState('');
   const [res, setRes] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const handlePromptChange = e => {
     setPromprt(e.target.value);
   };
   const handleSubmit = async () => {
+    setIsLoading(true);
     const result = await openai(prompt);
     setRes(result);
+    setIsLoading(false);
   };
 
   return (
@@ -23,7 +26,13 @@ function ChatGpt() {
           color={'white'}
         />
         <Button onClick={handleSubmit}>Submit</Button>
-        <Text color={'white'}>{res}</Text>
+        {isLoading ? (
+          <Spinner size={'xl'} color={'white'} />
+        ) : (
+          <Text color={'white'} fontFamily={'mono'}>
+            {res}
+          </Text>
+        )}
       </VStack>
     </Box>
   );
