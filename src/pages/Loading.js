fb482ec2c684326ba20dbc175f8d67074fd8911a
doodoodoong <doo_doong@hanmail.net>
2023-05-15 14:33:17 +0900

    loading, response routing page complete

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,14 +1,43 @@
-import { Box, Spinner } from '@chakra-ui/react';
-import LoadingMessage from '../components/LoadingMessage';
+import { Box, Flex, Spinner } from '@chakra-ui/react';
+import { TypeAnimation } from 'react-type-animation';
 
 function Loading() {
   return (
-    <>
-      <Box display={'flex'} bgColor={'black'} w={'100vw'} h={'100vh'}>
-        <Spinner size={'xl'} color={'white'} />
-        <LoadingMessage />
-      </Box>
-    </>
+    <Box
+      display={'flex'}
+      bgColor={'black'}
+      w={'100vw'}
+      h={'100vh'}
+      justifyContent={'center'}
+      alignItems={'center'}
+    >
+      <Spinner size={'xl'} color={'white'} />
+      <Flex justifyContent={'center'} alignItems={'center'}>
+        <TypeAnimation
+          sequence={[
+            '문제를 읽는중...',
+            1000,
+            '조건을 살펴보는 중...',
+            1000,
+            '답을 구하는 중...',
+            1000,
+            '검산하는 중...',
+            1000,
+          ]}
+          speed={50}
+          repeat={Infinity}
+          cursor={true}
+          style={{
+            marginLeft: '5px',
+            whiteSpace: 'pre-line',
+            fontSize: '2em',
+            display: 'inline',
+            fontFamily: 'NeoDunggeunmoPro-Regular',
+            color: 'white',
+          }}
+        />
+      </Flex>
+    </Box>
   );
 }
 
